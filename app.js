0c2912aa300c9f1c61ac45105cd05cddb6bd70ed
Murat Demirtas <73036794+muratdemirtas2001@users.noreply.github.com>
2021-01-10 21:39:46 +0000

    getting year,hours and minutes and injecting it to the html

--- app.js
+++ app.js
@@ -29,3 +29,15 @@ const items = document.querySelectorAll(".deadline-format h4");
 let futureDate = new Date(2021, 1, 22, 17, 30, 0);
 // console.log(typeof futureDate);
 // console.log(futureDate);
+const year = futureDate.getFullYear();
+// console.log(year);
+// console.log(typeof year);
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+
+let month = futureDate.getMonth();
+// console.log(month);
+month = months[month];
+// console.log(month);
+
+giveaway.textContent = `giveaway ends on ${month} ${year} ${hours}:${minutes}`;
